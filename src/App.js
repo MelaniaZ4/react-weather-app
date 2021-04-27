f2c979302cf6f1e524f2073dd27217fe26b2be85
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2021-04-28 10:16:06 +1200

    Changed margin of app and background, and set default search.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="weather-app-wrapper">
           
             <div className="weather-app">
-              <Weather city="Hamm"/>
+              <Weather defaultCity="Hamm"/>
             </div>
             <small>
               <a
