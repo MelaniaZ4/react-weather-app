aefe8afb529b26f990a0e04d32dfa352d8749ccd
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2021-04-30 11:47:36 +1200

    Included 5 day forecast (solution 1)

--- src/Forecast.js
+++ src/Forecast.js
@@ -24,6 +24,21 @@ if(loaded) {
           <div className="col">
             <WeatherForecastDay data={forecast[0]} />
           </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[5]} />
+          </div>
         </div>
       </div>
     );
