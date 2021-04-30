4fe766d971946098114ebc2aa071a6d68169b0b3
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2021-04-30 12:24:21 +1200

    Fixed warning

--- src/Forecast.js
+++ src/Forecast.js
@@ -27,6 +27,8 @@ if(loaded) {
               <WeatherForecastDay data={dailyForecast} />
               </div>
             )
+            } else { 
+              return null;
             }
           })}
         </div>
