72a4ce7dec2c44282d57bf244534d0828f44ff33
MelaniaZ4 <77643560+MelaniaZ4@users.noreply.github.com>
2021-04-30 12:46:02 +1200

    Change default city

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <div className="weather-app-wrapper">
           
             <div className="weather-app">
-              <Weather defaultCity="Hamm"/>
+              <Weather defaultCity="Auckland"/>
             </div>
             <small>
               <a
